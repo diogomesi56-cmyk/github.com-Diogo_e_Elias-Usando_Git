04d57e5e0bd79be796d012a6f0c6f9609059a1d2
Pegadinhaminecraft <elyasprofissa@gmail.com>
2026-08-30 00:19:19 -0300

    Criar javascript.js

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function votar(){
+    let concluido='Voto realizado com sucesso!!'
+    document.getElementById('concluido').innerHTML=concluido
+}
+button.addEventListener('click', votar)
+
+function toggleDarkMode() {  
+        const isDark = document.body.classList.toggle('dark-mode')
+        const logo = document.getElementById('logo')
+    
+}  
